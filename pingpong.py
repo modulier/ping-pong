7b22a701b8f779fd74390fbec0c1b1d536039a3e
modulier <khavanskiy_andreyka@mail.ru>
2025-04-20 12:50:32 +0300

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,79 @@
+from pygame import *
+
+
+
+
+
+#класс-родитель для спрайтов
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+       #конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height): # добавить еще два параметра при создании и задавать размер прямоугольгника для картинки самим
+        super().__init__()
+ 
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (wight, height)) # вместе 55,55 - параметры
+        self.speed = player_speed
+ 
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
+
+
+#Игровая сцена:
+back = (150, 200, 255) # цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+
+#флаги отвечающие за состояние игры
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player("racket.png",0,200,4,50,150)
+racket2 = Player("racket.png",550,200,4,50,150)
+ball = GameSprite("ball.png",200,200,4,50,50)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+   
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+       
+
+
+    display.update()
+    clock.tick(FPS)
+
+
